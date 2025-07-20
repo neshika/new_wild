38f87335f624be1c40a7c718d59326259832601c
neshika <stepanovaml@mail.ru>
2025-07-20 13:31:21 +0300

    добавлено меню

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -14,11 +14,28 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       bottomNavigationBar: Container(
         decoration: BoxDecoration(
-            borderRadius: BorderRadiusDirectional.only(
-              topStart: Radius.circular(20.0),
-              topEnd: Radius.circular(20.0),
+          borderRadius: BorderRadiusDirectional.only(
+            topStart: Radius.circular(20.0),
+            topEnd: Radius.circular(20.0),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withValues(alpha: 76), // 0.3 * 255 ≈ 76
+              spreadRadius: 1,
+              blurRadius: 5,
             ),
-            boxShadow: [BoxShadow(color: Colors.grey)]),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadiusDirectional.only(
+            topStart: Radius.circular(20.0),
+            topEnd: Radius.circular(20.0),
+          ),
+          child: BottomNavigationBar(items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: 'Saved'),
+          ]),
+        ),
       ),
     );
   }
