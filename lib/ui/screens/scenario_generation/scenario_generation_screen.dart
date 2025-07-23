24766fbfac66df1ff2d7654d4e200df9bd38c253
neshika <stepanovaml@mail.ru>
2025-07-23 17:02:48 +0300

    F06 sof

--- lib/ui/screens/scenario_generation/scenario_generation_screen.dart
+++ lib/ui/screens/scenario_generation/scenario_generation_screen.dart
@@ -21,11 +21,12 @@ class _ScenarioGenerationScreenState extends State<ScenarioGenerationScreen> {
   final videoLengthController = TextEditingController();
   final contentStyleThemeController = TextEditingController();
   final callToActionController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   @override
   void dispose() {
     //удаление контроллеров, для очистки памяти
-    // TODO: implement dispose
+
     super.dispose();
     videoThemeController.dispose();
     targetAudienceController.dispose();
@@ -42,76 +43,93 @@ class _ScenarioGenerationScreenState extends State<ScenarioGenerationScreen> {
         padding: EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           // оборачиваем колонку, чтобы можно было скроллить
-          child: Column(
-            children: [
-              // тема видео
-              ScenarioDescriptionTextfield(
-                title: 'Enter the theme of the video',
-                hint: 'Video theme',
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a video theme.';
-                  }
-                  return null;
-                },
-                controller: videoThemeController,
-              ),
-              const SizedBox(height: 16.0),
-              // аудитория
-              ScenarioDescriptionTextfield(
-                title: 'Enter the target audiens',
-                hint: 'target audiens',
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the target audiens.';
-                  }
-                  return null;
-                },
-                controller: targetAudienceController,
-              ),
-              const SizedBox(height: 16.0),
-              //длина видео
-              ScenarioDescriptionTextfield(
-                title: 'Enter the length of the video in second(15,30,60)',
-                hint: 'length of the video',
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the length of the video.';
-                  }
-                  if (int.tryParse(value) == null) {
-                    return 'Please enter a valid number.';
-                  }
-                  return null;
-                },
-                controller: videoLengthController,
-              ),
-              const SizedBox(height: 16.0),
-              // стиль контента
-              ScenarioDescriptionTextfield(
-                title: 'Enter the style of content',
-                hint: 'style content',
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the style content.';
-                  }
-                  return null;
-                },
-                controller: contentStyleThemeController,
-              ),
-              const SizedBox(height: 16.0),
-              // предлагаем действия: подписаться, комментировать
-              ScenarioDescriptionTextfield(
-                title: 'Enter a call action',
-                hint: 'call ti action',
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a call to action.';
-                  }
-                  return null;
-                },
-                controller: callToActionController,
-              ),
-            ],
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                // тема видео
+                ScenarioDescriptionTextfield(
+                  title: 'Enter the theme of the video',
+                  hint: 'Video theme',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a video theme.';
+                    }
+                    return null;
+                  },
+                  controller: videoThemeController,
+                ),
+                const SizedBox(height: 16.0),
+                // аудитория
+                ScenarioDescriptionTextfield(
+                  title: 'Enter the target audiens',
+                  hint: 'target audiens',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the target audiens.';
+                    }
+                    return null;
+                  },
+                  controller: targetAudienceController,
+                ),
+                const SizedBox(height: 16.0),
+                //длина видео
+                ScenarioDescriptionTextfield(
+                  title: 'Enter the length of the video in second(15,30,60)',
+                  hint: 'length of the video',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the length of the video.';
+                    }
+                    if (int.tryParse(value) == null) {
+                      return 'Please enter a valid number.';
+                    }
+                    return null;
+                  },
+                  controller: videoLengthController,
+                ),
+                const SizedBox(height: 16.0),
+                // стиль контента
+                ScenarioDescriptionTextfield(
+                  title: 'Enter the style of content',
+                  hint: 'style content',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the style content.';
+                    }
+                    return null;
+                  },
+                  controller: contentStyleThemeController,
+                ),
+                const SizedBox(height: 16.0),
+                // предлагаем действия: подписаться, комментировать
+                ScenarioDescriptionTextfield(
+                  title: 'Enter a call action',
+                  hint: 'call ti action',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a call to action.';
+                    }
+                    return null;
+                  },
+                  controller: callToActionController,
+                ),
+                const SizedBox(height: 24.0),
+                ElevatedButton(
+                  onPressed: () {
+                    formKey.currentState!.validate();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    //padding: EdgeInsetsDirectional.symmetric(vertical: 12.0),
+                    padding: EdgeInsetsDirectional.all(10.0),
+                  ),
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
