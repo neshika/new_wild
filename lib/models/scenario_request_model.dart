1f8734e0208366c71ffa966876416e346675da97
neshika <stepanovaml@mail.ru>
2025-08-07 06:39:56 +0300

    F08 ScenarioRequestModel

---
+++ lib/models/scenario_request_model.dart
@@ -0,0 +1,42 @@
+import 'package:new_wild/services/helpers.dart';
+
+class ScenarioRequestModel {
+  final SocialPlatform platform;
+  final String videoTheme;
+  final String targetAudience;
+  final int videoLengthInSecond;
+  final String contentStyle;
+  final String callToAction;
+
+  ScenarioRequestModel({
+    required this.platform,
+    required this.videoTheme,
+    required this.targetAudience,
+    required this.videoLengthInSecond,
+    required this.contentStyle,
+    required this.callToAction,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'platform': platform.toString(),
+      'videoTheme': videoTheme,
+      'targetAudience': targetAudience,
+      'videoLenghtInSecond': videoLengthInSecond,
+      'contentStyle': contentStyle,
+      'callToAction': callToAction,
+    };
+  }
+
+  static ScenarioRequestModel fromJson(Map<String, dynamic> json) {
+    return ScenarioRequestModel(
+      platform: SocialPlatform.values
+          .firstWhere((element) => element.toString() == json['platform']),
+      videoTheme: json['videoTheme'],
+      targetAudience: json['targetAudience'],
+      videoLengthInSecond: json['videoLengthInSecond'],
+      contentStyle: json['contentStyle'],
+      callToAction: json['callToAction'],
+    );
+  }
+}
