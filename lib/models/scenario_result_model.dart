bd09a4b1a511f746ae0c2f87afcea7b963a918ad
neshika <stepanovaml@mail.ru>
2025-08-07 06:52:13 +0300

    F08 ScenarioResultModel

---
+++ lib/models/scenario_result_model.dart
@@ -0,0 +1,33 @@
+import 'package:new_wild/models/scenario_request_model.dart';
+
+class ScenarioResultModel {
+  final String id;
+  final String title;
+  final String body;
+  final ScenarioRequestModel request;
+
+  ScenarioResultModel({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.request,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'body': body,
+      'request': request.toJson(),
+    };
+  }
+
+  static ScenarioResultModel fromJson(Map<String, dynamic> json) {
+    return ScenarioResultModel(
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+      request: ScenarioRequestModel.fromJson(json['videoLengthInSecond']),
+    );
+  }
+}
